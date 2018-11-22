68612df663c9c5fcdfe43857ff277613b4bc5c6a
Filippo maria COPPOLA <ficoppol@e3r13p1.42.fr>
2018-11-22 18:01:06 +0100

    cleaning

--- mains/strtrim.c
+++
@@ -1,9 +0,0 @@
-#include "../includes/libft.h"
-#include <stdio.h>
-
-int		main(int argc, char **argv)
-{
-		printf("before: %s\n", argv[1]);
-		printf("-after: %s\n", ft_strtrim(argv[1]));
-		return (1);
-}
